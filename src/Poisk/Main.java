d7a7127e958ae81645fbc1395d254b7cbb970f5f
Maks <maks105fmm@gmail.com>
2022-10-07 12:12:48 +0300

    Min number Max number

--- src/Poisk/Main.java
+++ src/Poisk/Main.java
@@ -4,13 +4,22 @@ public class Main {
     public static void main(String[] args) {
         int[] myArray = {2, 2, 3, 4, 5, 6, 78, 1, 4, 3, 2, 5, 7, 44};
         int min = myArray[0];
+        int max = myArray[0];
+
         for (int i = 0; i < myArray.length; i++) {
             if (min > myArray[i]) {
                 min = myArray[i];
             }
 
         }
-        System.out.println(min);
+        for (int i = 0; i < myArray.length; i++) {
+            if (max < myArray[i]) {
+                max = myArray[i];
+            }
+
+        }
+        System.out.println("Минимальное число: " + min);
+        System.out.println("Максимальное число: " + max);
 
     }
 }
